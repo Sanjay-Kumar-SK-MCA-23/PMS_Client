372d95a10a138f04c3f643693c21e8ff1ce1d214
Sanjay Kumar M <139735109+Sanjay-Kumar-SK-MCA-23@users.noreply.github.com>
2025-03-24 12:00:10 +0530

    Update axiosConfig.js

--- src/components/utils/axiosConfig.js
+++ src/components/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://pms-server-b55d.onrender.com/api";
-export const baseURL_Image = "http://pms-server-b55d.onrender.com/api";
+export const baseURL = "https://pms-server-b55d.onrender.com/api";
+export const baseURL_Image = "https://pms-server-b55d.onrender.com/api";
 
 export const instance = axios.create({
   baseURL: baseURL,
